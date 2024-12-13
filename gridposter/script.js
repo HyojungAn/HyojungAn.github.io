c1de76d4d8f13d6a49bcb75f0a76c52a08b19193
HyojungAn <han10@sva.edu>
2024-12-13 14:44:30 -0500

    git

--- gridposter/script.js
+++ gridposter/script.js
@@ -6,30 +6,35 @@ const circleGroup = document.getElementById('circleGroup');
 svgElement.addEventListener('click', () => {
     // 'rotate' 클래스 토글
     svgElement.classList.toggle('rotate');
-    // 원들의 색상 변경
-    const circles = circleGroup.getElementsByTagName('circle');
-    for (let i = 0; i < circles.length; i++) {
-        const currentColor = circles[i].getAttribute('stroke');
-        let newColor;
 
-        switch (currentColor) {
-            case 'red':
-                newColor = 'blue';
-                break;
-            case 'blue':
-                newColor = 'green';
-                break;
-            case 'green':
-                newColor = 'orange';
-                break;
-            case 'orange':
-                newColor = 'purple';
-                break;
-            case 'purple':
-                newColor = 'red';
-                break;
+    // 각 세트의 색상 변경
+    const circleSets = circleGroup.getElementsByClassName('circleSet');
+    for (let j = 0; j < circleSets.length; j++) {
+        const circles = circleSets[j].getElementsByTagName('circle');
+        for (let i = 0; i < circles.length; i++) {
+            const currentColor = circles[i].getAttribute('stroke');
+            let newColor;
+
+            switch (currentColor) {
+                case 'red':
+                    newColor = 'blue';
+                    break;
+                case 'blue':
+                    newColor = 'green';
+                    break;
+                case 'green':
+                    newColor = 'orange';
+                    break;
+                case 'orange':
+                    newColor = 'purple';
+                    break;
+                case 'purple':
+                    newColor = 'red';
+                    break;
+            }
+
+            circles[i].setAttribute('stroke', newColor);
         }
-
-        circles[i].setAttribute('stroke', newColor);
     }
 });
+
